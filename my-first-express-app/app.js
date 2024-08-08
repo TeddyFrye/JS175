25860a590e9169b5e2a9bed79b8155430f888bb8
TeddyFrye <111472179+TeddyFrye@users.noreply.github.com>
2024-08-08 13:15:03 -0500

    Adds other languages and footer navigation

--- my-first-express-app/app.js
+++ my-first-express-app/app.js
@@ -1,15 +1,31 @@
 const express = require(`express`);
-
+const morgan = require("morgan");
 const app = express();
 
+app.set("views", "./views");
 app.set(`view engine`, `pug`);
 
-app.get(`/`, (request, response) => {
-  response.render(`index`);
+app.use(express.static("public"));
+app.use(morgan("common"));
+
+app.get(`/`, (req, res) => {
+  res.render(`index`);
 });
 
-app.get(`/account`, (request, response) => {
-  response.render(`account`, { money: `$16,000`, recentTransaction: true });
+app.get(`/account`, (req, res) => {
+  res.render(`account`, { money: `$16,000`, recentTransaction: true });
+});
+
+app.get(`/account-english`, (req, res) => {
+  res.redirect("/account");
+});
+
+app.get(`/account-french`, (req, res) => {
+  res.render(`account-french`, { money: `$16,000`, recentTransaction: true });
+});
+
+app.get(`/account-serbian`, (req, res) => {
+  res.render(`account-serbian`, { money: `$16,000`, recentTransaction: false });
 });
 
 app.listen(3000, () => {
